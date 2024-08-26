2eb089dfd7262d7f09cf31c490273362c9c47f1a
chetansirohi <chetansirohi36@gmail.com>
2024-08-26 23:45:41 +0530

    'fixed-mpage'

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,7 +5,6 @@ export default function Main() {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="flex flex-col md:flex-row items-center justify-center gap-8">
-        {/* Text Column */}
         <div className="flex-1 flex items-center justify-center text-center ">
           <div>
             <h1 className="md:text-6xl font-bold mb-4">
@@ -17,11 +16,10 @@ export default function Main() {
             </p>
           </div>
         </div>
-        {/* Image Column */}
         <div className="flex-1 flex items-center justify-center">
           <Image
             src="/assets/logo.png"
-            alt="Product"
+            alt="Chatty Logo"
             width={270}
             height={270}
             className="max-w-md w-full h-auto"
